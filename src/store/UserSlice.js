ace241d5e5da6e15d7af1b6691ae47fc35d84508
Hrishikesh Y Joshi <hrishikesh.joshi1@in.ey.com>
2024-09-12 22:16:52 +0530

    attempt with staticwebapp.config.json

--- src/store/UserSlice.js
+++ src/store/UserSlice.js
@@ -86,7 +86,7 @@ export const loginUser = createAsyncThunk(
       // client = new Client(auth, URL);
       // client.setAuth(auth);
       // const fetchClient = client.core;
-      client = await Client.authenticateViaOAuthInternal({user: creds.email, password: creds.password}, "https://dev.hennypenny.com");
+      client = await Client.authenticateViaOAuthInternal({user: creds.email, password: creds.password}, URL);
       // let auth = new CookieAuth();
       
       user = await client.user.currentWithEffectiveRoles();
